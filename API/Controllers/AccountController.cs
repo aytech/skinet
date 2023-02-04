f759c670b839b940402b6e3012fa2fac37e052d8
Oleg <oyapparov@gmail.com>
2023-02-04 10:49:54 +0100

    Section 15 in progress, add login controller and DTOs

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+using API.Dtos;
+using API.Errors;
+using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly SignInManager<AppUser> signInManager;
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null)
+            {
+                return Unauthorized(new ApiResponse(401));
+            }
+
+            var result = await signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded)
+            {
+                return Unauthorized(new ApiResponse(401));
+            }
+
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = "This will be a token",
+                DisplayName = user.DisplayName
+            };
+        }
+    }
+}
