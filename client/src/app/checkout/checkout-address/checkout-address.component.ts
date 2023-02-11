68d70232bfd9598bdc24249d4d1872af885dc4c5
Oleg <oyapparov@gmail.com>
2023-02-11 10:44:21 +0100

    Section 19 in progress, enable save as default address option

--- client/src/app/checkout/checkout-address/checkout-address.component.ts
+++ client/src/app/checkout/checkout-address/checkout-address.component.ts
@@ -1,13 +1,28 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from 'src/app/account/account.service';
 
-@Component({
+@Component( {
   selector: 'app-checkout-address',
   templateUrl: './checkout-address.component.html',
-  styleUrls: ['./checkout-address.component.scss']
-})
+  styleUrls: [ './checkout-address.component.scss' ]
+} )
 export class CheckoutAddressComponent {
 
   @Input() checkoutForm!: FormGroup
 
+  constructor( private accountService: AccountService, private toastrService: ToastrService ) { }
+
+  saveUserAddress() {
+    this.accountService
+      .updateUserAddress( this.checkoutForm.get( "addressForm" )?.value )
+      .subscribe( {
+        next: _ => this.toastrService.success( "Address saved" ),
+        error: error => {
+          console.log( error )
+          this.toastrService.error( "Could not save the address" )
+        }
+      } )
+  }
 }
