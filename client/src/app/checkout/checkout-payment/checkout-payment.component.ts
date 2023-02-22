7bc2e8b425d078b3afc035cc9d7709189e584371
Oleg <oyapparov@gmail.com>
2023-02-22 09:01:02 +0100

    Section 21 in progress, update loading indicator for payments

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -26,6 +26,7 @@ export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
   cardCvc: any
   cardErrors: any
   cardHandler = this.onChange.bind( this )
+  loading: boolean = false
 
   constructor( private basketService: BasketService, private checkoutService: CheckoutService, private toastr: ToastrService, private router: Router ) { }
 
@@ -60,38 +61,44 @@ export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
     }
   }
 
-  submitOrder() {
+  async submitOrder() {
+    this.loading = true
     const basket = this.basketService.getCurrentBasketValue()
+
     if ( basket !== null ) {
-      const orderToCreate = this.getOrderToCreate( basket )
-      this.checkoutService.createOrder( orderToCreate )
-        .subscribe( {
-          next: ( order: IOrder ) => {
-            this.toastr.success( 'Order created successfully' )
-            this.stripe.confirmCardPayment( basket.clientSecret, {
-              payment_method: {
-                card: this.cardNumber,
-                billing_details: {
-                  name: this.checkoutForm.get( 'paymentForm' )?.get( 'nameOnCard' )?.value
-                }
-              }
-            } ).then( ( result: any ) => {
-              console.info( result )
-              if ( result.paymentIntent ) {
-                this.basketService.deleteLocalBasket( basket.id )
-                const navigationExtras: NavigationExtras = { state: order }
-                this.router.navigate( [ "checkout/success" ], navigationExtras )
-              } else {
-                this.toastr.error( result.error.message )
-              }
-            } )
-          },
-          error: error => {
-            console.error( error )
-            this.toastr.error( "Could not create order" )
-          }
-        } )
+      try {
+        const createdOrder = await this.createOrder( basket )
+        const paymentResults = await this.confirmPaymentWithStripe( basket )
+
+        if ( paymentResults.paymentIntent ) {
+          this.basketService.deleteLocalBasket( basket.id )
+          const navigationExtras: NavigationExtras = { state: createdOrder }
+          this.router.navigate( [ "checkout/success" ], navigationExtras )
+        } else {
+          this.toastr.error( paymentResults.error.message )
+        }
+        this.loading = false
+      } catch ( error ) {
+        console.log( error )
+        this.loading = false
+      }
     }
+  }
+
+  private async confirmPaymentWithStripe( basket: IBasket ) {
+    return this.stripe.confirmCardPayment( basket.clientSecret, {
+      payment_method: {
+        card: this.cardNumber,
+        billing_details: {
+          name: this.checkoutForm.get( 'paymentForm' )?.get( 'nameOnCard' )?.value
+        }
+      }
+    } )
+  }
+
+  private async createOrder( basket: IBasket ) {
+    const orderToCreate = this.getOrderToCreate( basket )
+    return this.checkoutService.createOrder( orderToCreate ).toPromise()
   }
 
   private getOrderToCreate( basket: IBasket ): IOrderToCreate {
